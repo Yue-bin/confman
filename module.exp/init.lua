ce32f89af40f987d3f881db5e2a25e70f1e8aabe
Yue-bin <3103174284@qq.com>
2026-02-19 00:30:51 +0800

    示例模块配置

--- module.exp/init.lua
+++ module.exp/init.lua
@@ -1,3 +1,36 @@
 local _M = {}
 
+-- 安装模块，按顺序执行，每项一个table
+_M.install = {
+    {
+        name = "install config", -- 名字，日志会用到
+        action = "cp",           -- 执行的动作
+        content = {
+            src = "expmod.conf", -- 采用同级目录路径，会在其它地方处理
+            dst = "/etc/expmod/expmod.conf"
+        }
+    },
+    {
+        -- 这会变成 `systemctl restart expmod`
+        name = "restart expmod",
+        action = "systemd",
+        content = {
+            action = "restart",
+            service = "expmod"
+        }
+    },
+    {
+        name = "some commands",
+        action = "cmd",
+        content = {
+            {
+                "echo 'hello world'", -- 这会被当成shell命令执行
+            },
+            {
+                "ls -l /etc/expmod/expmod.conf"
+            }
+        }
+    }
+}
+
 return _M
