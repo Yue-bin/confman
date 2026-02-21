c5761fe0ce57b6cffdf75106b0f11b489a920d2c
Yue-bin <3103174284@qq.com>
2026-02-21 13:00:32 +0800

    📃 docs(module.exp): 完善示例
    添加了install和remove的示例

--- module.exp/init.lua
+++ module.exp/init.lua
@@ -1,12 +1,49 @@
 local _M = {}
 
--- 安装模块，按顺序执行，每项一个table
+-- 安装模块
 _M.install = {
     {
+        name = "install expmod",
+        action = "install_pkgs",
+        content = {
+            "expmod" -- 包名，可以添加多个
+        }
+    }
+}
+
+-- 配置模块，按顺序执行，每项一个table
+_M.apply = {
+    {
         name = "install config", -- 名字，日志会用到
+        pre =                    -- 可选的预操作，是一个action，而且可以无限套娃
+        {
+            name = "check if config file exists",
+            pre = { -- 套娃
+                name = "soft fail",
+                action = "soft",
+                content = {
+                    name = "check if im sudo",
+                    action = "cmd",
+                    content = {
+                        "id -u | grep -q 0"
+                    }
+                }
+            },
+            action = "testfile",
+            content = {
+                path = "expmod.conf"
+            }
+        },
+        post = { -- 可选的后检查，格式同precheck
+            name = "check if config file was copied",
+            action = "testfile",
+            content = {
+                path = "/etc/expmod/expmod.conf"
+            }
+        },
         action = "cp",           -- 执行的动作
         content = {
-            src = "expmod.conf", -- 采用同级目录路径，会在其它地方处理
+            src = "expmod.conf", -- 采用相对路径
             dst = "/etc/expmod/expmod.conf"
         }
     },
@@ -23,8 +60,18 @@ _M.install = {
         name = "some commands",
         action = "cmd",
         content = {
-            "echo 'hello world'",     -- 这会被当成shell命令执行
+            "echo 'hello world'", -- 这会被当成shell命令执行
             "ls -l /etc/expmod/expmod.conf"
+        }
+    }
+}
+
+_M.remove = {
+    {
+        name = "remove expmod",
+        action = "remove_pkgs",
+        content = {
+            "expmod" -- 包名，可以添加多个
         }
     }
 }
