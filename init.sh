36915251acd70e725b5ce18d84e92d4963c68d31
Yue-bin <3103174284@qq.com>
2026-02-21 16:58:57 +0800

    🐳 chore(init): 使用luarocks
    byd apt，装出来的版本参差不齐的

--- init.sh
+++ init.sh
@@ -2,9 +2,11 @@
 set -euo pipefail
 
 # 暂时只适配debian系
-apt install -y \
-    lua5.4 \
-    lua-logging \
-    lua-posix \
-    lua-ansicolors \
-    lua-filesystem
+apt install -y lua5.4 \
+    luarocks
+
+luarocks-5.4 install \
+    lualogging \
+    luaposix \
+    ansicolors \
+    luafilesystem
